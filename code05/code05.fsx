0d905040c33400371d9105662448c9f208d8b1b8
David Bragmark <david.bragmark@valtech.com>
2020-12-09 09:55:00 +0100

    aoc 5

---
+++ code05/code05.fsx
@@ -0,0 +1,36 @@
+let readLines filePath = System.IO.File.ReadLines(filePath);;
+
+let data =
+  readLines "input05.txt"
+  |> Seq.toList
+
+let strRev α=let N=System.Globalization.StringInfo.GetTextElementEnumerator(α)
+             List.unfold(fun n->if n then Some(N.GetTextElement(),N.MoveNext()) else None)(N.MoveNext())|>List.rev|>String.concat ""
+
+
+let parseChar char =
+  match char with
+    | 'B' -> '1'
+    | 'F' -> '0'
+    | 'R' -> '1'
+    | 'L' -> '0'
+
+
+let seatIds =
+  data
+  |> List.map (fun line -> line |> String.map parseChar)
+  |> List.map (fun line -> (line.[0..6], line.[7..9]))
+  |> List.map (fun (row, column) -> (System.Convert.ToInt32(row, 2), System.Convert.ToInt32(column, 2)))
+  |> List.map (fun (row, column) -> row * 8 + column )
+  |> List.sort
+
+let taskA = seatIds |> List.max
+
+
+let taskB=
+  seatIds
+  |> List.pairwise
+  |> List.find (fun (a,b) -> a <> b-1)
+  |> fun (a,b) -> (a+b)/2
+
+printfn "This is input: %A" input
