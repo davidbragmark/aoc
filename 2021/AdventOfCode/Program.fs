43afcc458a2bac95535696102843bfa1e533ee05
David Bragmark <david.bragmark@valtech.com>
2021-12-02 07:32:55 +0100

    AdventOfCode 2021 Day 2

--- 2021/AdventOfCode/Program.fs
+++ 2021/AdventOfCode/Program.fs
@@ -17,14 +17,40 @@ let solveDay01_2 (input: int []) =
   |> Array.map Array.sum
   |> solveDay01_1
 
+let solveDay02_1 (input: (string * int64) []) =
+  input
+  |> Seq.ofArray
+  |> Seq.fold (fun (state: int64 * int64) step ->
+      let vert, horizontal = state
+      match step with
+      | "forward", x -> (vert, horizontal + x)
+      | "up", x      -> (vert - x, horizontal)
+      | "down", x    -> (vert + x, horizontal)
+      | _ -> failwith "should not happen"
+    )
+    (0L, 0L)
+  |> fun (x, y) -> x * y
+
+let solveDay02_2 (input: (string * int64) []) =
+  input
+  |> Seq.ofArray
+  |> Seq.fold (fun (state: int64 * int64 * int64) step ->
+      let vert, horizontal, aim = state
+      match step with
+      | "forward", x -> (vert + aim * x, horizontal + x, aim)
+      | "up", x      -> (vert, horizontal, aim - x)
+      | "down", x    -> (vert, horizontal, aim + x)
+      | _ -> failwith "should not happen"
+    )
+    (0L, 0L, 0L)
+  |> fun (x, y, _) -> x * y
+
 [<EntryPoint>]
 let main argv =
-    let day01p1 = solveDay01_1 Input.Day01
-    let day01p2 = solveDay01_2 Input.Day01
-    //let aoc = solveDay01 testInput
+    let day02p1 = solveDay02_1 Input.Day02.input
+    let day02p2 = solveDay02_2 Input.Day02.input
 
-    //printfn "Hello world %A" aoc
-    printfn "Day 01 part 1: %A" day01p1
-    printfn "Day 01 part 2: %A" day01p2
+    printfn "Day 02 part 1: %A" day02p1
+    printfn "Day 02 part 2: %A" day02p2
 
     0 // return an integer exit code
