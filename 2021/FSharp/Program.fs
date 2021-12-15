0ccdd222ad9f1f3a7fa90ba69aae14411e3ac3b8
David Bragmark <david.bragmark@valtech.com>
2021-12-15 09:14:30 +0100

    Add Day04 Part1 and Part2

--- 2021/FSharp/Program.fs
+++ 2021/FSharp/Program.fs
@@ -1,21 +1,29 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-
 open System
 
 open AdventOfCode
 
 [<EntryPoint>]
 let main argv =
-    let day01p1 = Solution.Day01.part1 Input.Day01.input
-    let day01p2 = Solution.Day01.part2 Input.Day01.input
-    let day02p1 = Solution.Day02.part1 Input.Day02.input
-    let day02p2 = Solution.Day02.part2 Input.Day02.input
+    let input04 = System.IO.File.ReadAllText "Input/input04.txt"
 
-    printfn "Day 01 part 1: %A"   day01p1
-    printfn "Day 01 part 2: %A\n" day01p2
+    //let day01p1 = Solution.Day01.part1 Input.Day01.input
+    //let day01p2 = Solution.Day01.part2 Input.Day01.input
+    //let day02p1 = Solution.Day02.part1 Input.Day02.input
+    //let day02p2 = Solution.Day02.part2 Input.Day02.input
+    //let day03p1 = Solution.Day03.part1
+    //let day03p2 = Solution.Day03.part2
 
-    printfn "Day 02 part 1: %A"   day02p1
-    printfn "Day 02 part 2: %A\n" day02p2
+    let day04 = Solution.Day04.solve input04
 
+    printfn $"Day 04: %A{day04}"
+
+    //printfn "Day 01 part 1: %A"   day01p1
+    //printfn "Day 01 part 2: %A\n" day01p2
+
+    //printfn "Day 02 part 1: %A"   day02p1
+    //printfn "Day 02 part 2: %A\n" day02p2
+
+    //printfn "Day 03 part 1: %A"   day03p1
+    //printfn "Day 03 part 2: %A\n" day03p2
     0 // return an integer exit code
---
+++ 2021/FSharp/Solution/Day04.fs
@@ -0,0 +1,117 @@
+namespace AdventOfCode.Solution
+
+open AdventOfCode
+open System.Text.RegularExpressions
+
+type DrawnBoardNumber = { number: int; drawn: bool }
+
+type Board =
+  { board: DrawnBoardNumber list list
+    won: bool
+    count: int
+    winningNumber: int option }
+
+type Selector = (Board -> int) -> Board list -> Board
+
+module Day04 =
+  let parseNumber (char: string) (line: string) =
+    Regex.Split(line, char)
+    |> List.ofArray
+    |> List.map int
+
+  let initializeBoard (board: int list list) =
+    let board =
+      board
+      |> List.map (fun line ->
+        line
+        |> List.map (fun item -> { number = item; drawn = false }))
+
+    { board = board
+      won = false
+      count = 0
+      winningNumber = None }
+
+  let parseBoard (lines: string list) =
+    lines
+    |> List.map (fun line -> line.Trim())
+    |> List.map (fun line -> parseNumber "\s+" line)
+    |> initializeBoard
+
+  let parse (input: string) =
+    let data =
+      input.Split "\n"
+      |> List.ofArray
+      |> List.filter (System.String.IsNullOrWhiteSpace >> not)
+
+    let numbers = data |> List.head |> parseNumber ","
+
+    let boards =
+      data
+      |> List.skip 1
+      |> List.chunkBySize 5
+      |> List.map parseBoard
+
+    numbers, boards
+
+  let hasWon (board: DrawnBoardNumber list list) =
+    let winCondition = fun board ->
+     board
+     |> List.exists (fun line ->
+       line
+       |> List.forall (fun drawnNumber -> drawnNumber.drawn = true))
+
+    let p1 = board |> List.transpose |> winCondition
+    let p2 = board |> winCondition
+
+    p1 || p2
+
+  let drawOnBoard (number: int) (board: Board) =
+    let brd =
+      board.board
+      |> List.map (fun line ->
+        line
+        |> List.map (fun item ->
+          if item.number = number then
+            { item with drawn = true }
+          else
+            item))
+    let won = hasWon brd
+
+    { board with
+        board = brd
+        won = hasWon brd
+        count = board.count + 1
+        winningNumber = match won with | true -> Some number | false -> None }
+
+  let rec drawNumber (numbers: int list) (board: Board) =
+    match numbers with
+    | _ when board.won -> board // Dont draw more on board if won
+    | [] -> board
+    | nr::xs -> drawNumber xs (drawOnBoard nr board)
+
+  let score (board: Board) =
+    let unmarked =
+      board.board
+      |> List.concat
+      |> List.filter (fun item -> item.drawn = false)
+      |> List.sumBy (fun item -> item.number)
+    unmarked * board.winningNumber.Value
+
+  let play (selector: Selector) (input: int list * Board list) =
+    let numbers, boards = input
+    boards
+    |> List.map (drawNumber numbers)
+    |> List.filter (fun board -> board.won)
+    |> selector (fun board -> board.count)
+    |> score
+
+  let part1 (input: int list * Board list)=
+    play List.minBy input
+
+  let part2 (input: int list * Board list)=
+    play List.maxBy input
+
+  let solve input =
+    parse input
+    |> part1
+
